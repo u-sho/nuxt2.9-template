023cda5b6fa8cdfbfef0b990b165483ff9b388cd
Shohei "u-sho" Uechi <shouhei.uechi@gmail.com>
2019-12-19 18:23:02 +0900

    Update stylelint.config.js
    remove scss plugin

--- template/stylelint.config.js
+++ template/stylelint.config.js
@@ -2,5 +2,4 @@ module.exports = {
   // https://stylelint.io/user-guide/configuration
   ignorePath: '.gitignore',
   extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
-  plugins: ['stylelint-scss']
 }
